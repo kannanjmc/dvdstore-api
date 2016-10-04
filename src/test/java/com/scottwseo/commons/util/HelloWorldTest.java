0271b4e158de0d7ed305183809eb1ab650bb78f8
Scott Seo <scott.seo@gmail.com>
2016-10-04 08:24:54 -0400

    adding hello world test for jenkins job test

---
+++ src/test/java/com/scottwseo/commons/util/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package com.scottwseo.commons.util;
+
+import org.junit.Test;
+
+/**
+ * Created by seos on 10/4/16.
+ */
+public class HelloWorldTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello World");
+    }
+
+}
